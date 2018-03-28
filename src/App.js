2c54a6f2f21afcf973a5bf1e1ca282d30c392cbd
Wagner Faria do Amaral <wagneramaral@sedec.mt.gov.br>
2018-03-28 03:25:59 -0400

    Adicionando referencia de metodos da classe para o componente

--- src/App.js
+++ src/App.js
@@ -13,13 +13,13 @@ class App extends Component {
     otherState: 'Some other value'
   }
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     // console.log('Was clicked!')
     // DONT DO THIS this.state.persons[0].name = 'Wagner Faria do Amaral';
     this.setState(
       {
         persons: [
-          { name: 'Wagner Faria do Amaral', age: 33 },
+          { name: newName, age: 33 },
           { name: 'Dejaine', age: 29 },
           { name: 'Danilo', age: 33 }
         ]
@@ -27,15 +27,37 @@ class App extends Component {
     )
   }
 
+  nameChangedHandler = (event) => {
+    this.setState(
+      {
+        persons: [
+          { name: 'Wagner', age: 33 },
+          { name: event.target.value, age: 29 },
+          { name: 'Danilo', age: 32 }
+        ]
+      }
+    )
+  }
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is realy working !</p>
-        <button onClick={this.switchNameHandler}>Switch name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+        {/* ESTA FORMA DE PASSAR PARAMETROS PARA UM METODO EH INEFICIENTE */}
+        <button onClick={() => this.switchNameHandler('Wagner Faria do Amaral')}>Switch name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age} />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, 'Wagner Amaral')}
+          changed={this.nameChangedHandler}>
+          My Hobbies: Tricotar
+        </Person>
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age} />
       </div>
     );
   }
