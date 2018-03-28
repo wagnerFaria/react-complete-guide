c8c73a50d16c237145d544ebcabafb307b60162e
Wagner Faria do Amaral <wagneramaral@sedec.mt.gov.br>
2018-03-28 03:38:49 -0400

    Aula 47 - Introdução a estilos css

--- src/App.js
+++ src/App.js
@@ -39,12 +39,20 @@ class App extends Component {
     )
   }
   render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is realy working !</p>
         {/* ESTA FORMA DE PASSAR PARAMETROS PARA UM METODO EH INEFICIENTE */}
-        <button onClick={() => this.switchNameHandler('Wagner Faria do Amaral')}>Switch name</button>
+        <button style={style} onClick={() => this.switchNameHandler('Wagner Faria do Amaral')}>Switch name</button>
         <Person
           name={this.state.persons[0].name}
           age={this.state.persons[0].age} />
