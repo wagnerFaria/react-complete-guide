453e03d9d78afe2e5a711ddee4970a39c35d0ced
Wagner Faria do Amaral <wagneramaral@sedec.mt.gov.br>
2018-03-28 02:55:10 -0400

    Aula 42 - Introdução a variavel state

--- src/App.js
+++ src/App.js
@@ -3,14 +3,39 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+
+  state = {
+    persons: [
+      { name: 'Wagner', age: 33 },
+      { name: 'Dejaine', age: 29 },
+      { name: 'Danilo', age: 32 }
+    ],
+    otherState: 'Some other value'
+  }
+
+  switchNameHandler = () => {
+    // console.log('Was clicked!')
+    // DONT DO THIS this.state.persons[0].name = 'Wagner Faria do Amaral';
+    this.setState(
+      {
+        persons: [
+          { name: 'Wagner Faria do Amaral', age: 33 },
+          { name: 'Dejaine', age: 29 },
+          { name: 'Danilo', age: 33 }
+        ]
+      }
+    )
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is realy working !</p>
-        <Person name="Wagner" age="33" />
-        <Person name="Dejaine" age="29">My hobbies: tricotar</Person>
-        <Person name="Danilo" age="32" />
+        <button onClick={this.switchNameHandler}>Switch name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
     );
   }
