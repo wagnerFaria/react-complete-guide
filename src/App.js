92ba73eb87c310aad32910f9fe3f80f997634a06
Wagner Faria do Amaral <wagneramaral@sedec.mt.gov.br>
2018-03-29 01:01:17 -0400

    Aula 65 - Utilização do Radium para usar CSS

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
+import Radium, { StyleRoot } from 'radium';
 
 class App extends Component {
 
@@ -48,11 +49,16 @@ class App extends Component {
 
   render() {
     const style = {
-      backgroundColor: 'white',
+      backgroundColor: 'green',
+      color: 'white',
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
     };
 
     let persons = null;
@@ -70,18 +76,32 @@ class App extends Component {
           })}
         </div>
       );
+      style.backgroundColor = 'red';
+      style[':hover'] = {
+        backgroundColor: 'salmon',
+        color: 'black'
+      }
     }
 
+    const classes = [];
+    if (this.state.persons.length <= 2) {
+      classes.push('red'); // classes = ['red'];
+    }
+    if (this.state.persons.length <= 1) {
+      classes.push('bold'); // classes = ['red'];
+    }
     return (
-      <div className="App">
-        <h1>Hi, I'm a React App</h1>
-        <p>This is realy working !</p>
-        {/* ESTA FORMA DE PASSAR PARAMETROS PARA UM METODO EH INEFICIENTE */}
-        <button style={style} onClick={this.togglePersonsHandler}>Switch name</button>
-        {persons}
-      </div>
+      <StyleRoot >
+        <div className="App">
+          <h1>Hi, I'm a React App</h1>
+          <p className={classes.join(' ')}>This is realy working !</p>
+          {/* ESTA FORMA DE PASSAR PARAMETROS PARA UM METODO EH INEFICIENTE */}
+          <button style={style} onClick={this.togglePersonsHandler}>Switch name</button>
+          {persons}
+        </div>
+      </StyleRoot>
     );
   }
 }
 
-export default App;
+export default Radium(App);
