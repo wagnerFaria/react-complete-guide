81d153eb9e2e2eb2c78ed244539e846fc43bfc5f
Wagner Faria do Amaral <wagneramaral@sedec.mt.gov.br>
2018-03-29 01:58:52 -0400

    Aula 70 - Adicionado CSS Modules config (muito bom)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-import './App.css';
+import classes from './App.css';
 import Person from './Person/Person';
-import Radium, { StyleRoot } from 'radium';
 
 class App extends Component {
 
@@ -48,20 +47,8 @@ class App extends Component {
   }
 
   render() {
-    const style = {
-      backgroundColor: 'green',
-      color: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer',
-      ':hover': {
-        backgroundColor: 'lightgreen',
-        color: 'black'
-      }
-    };
-
     let persons = null;
+    let btnClass = '';
 
     if (this.state.showPersons) {
       persons = (
@@ -76,32 +63,25 @@ class App extends Component {
           })}
         </div>
       );
-      style.backgroundColor = 'red';
-      style[':hover'] = {
-        backgroundColor: 'salmon',
-        color: 'black'
-      }
+      btnClass = classes.Red;
     }
 
-    const classes = [];
+    const assignedClasses = [];
     if (this.state.persons.length <= 2) {
-      classes.push('red'); // classes = ['red'];
+      assignedClasses.push(classes.red); // classes = ['red'];
     }
     if (this.state.persons.length <= 1) {
-      classes.push('bold'); // classes = ['red'];
+      assignedClasses.push(classes.bold); // classes = ['red'];
     }
     return (
-      <StyleRoot >
-        <div className="App">
-          <h1>Hi, I'm a React App</h1>
-          <p className={classes.join(' ')}>This is realy working !</p>
-          {/* ESTA FORMA DE PASSAR PARAMETROS PARA UM METODO EH INEFICIENTE */}
-          <button style={style} onClick={this.togglePersonsHandler}>Switch name</button>
-          {persons}
-        </div>
-      </StyleRoot>
+      <div className={classes.App}>
+        <h1>Hi, I'm a React App</h1>
+        <p className={assignedClasses.join(' ')}>This is realy working !</p>
+        <button className={btnClass} onClick={this.togglePersonsHandler}>Switch name</button>
+        {persons}
+      </div>
     );
   }
 }
 
-export default Radium(App);
+export default App;
